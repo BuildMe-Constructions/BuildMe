fe592e931398db09822a303ebf2aadc4d46f3789
Prasanth <31370970+prasanthse@users.noreply.github.com>
2019-09-07 20:59:06 +0530

    Get Faqs and Display logic finished

--- FrontEnd/Buildme/src/app/components/faq/faq.component.ts
+++ FrontEnd/Buildme/src/app/components/faq/faq.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BackendServiceService } from '../../Services/backend-service.service'; //Import Backend Service
+import { Faq } from '../../Classes/faq'; //Import Faq class
+
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+  urlGetFaq = "http://localhost:4200/Faq/uploadFAQ"; //API for get all Faq questions and answers
+  errorMessage = "";
+
+  public faqs: Faq[] = [];
+
+  constructor(private backend: BackendServiceService) { }
 
   ngOnInit() {
+    this.GetAllFQAs();//Call function to get the FAQs while initializing the component
+  }
+
+  //Function to get all the FAQs
+  GetAllFQAs(){
+    this.backend.getRequest(this.urlGetFaq).subscribe(message => {
+      this.faqs = message; //Assign the response to FAQs object array 
+    }, error => this.errorMessage = error);
   }
 
 }
